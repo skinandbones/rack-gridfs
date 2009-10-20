a88801de0e319d656530afcb678093ac79ae275a
Blake Carlson <blc@punchbowlsoftware.com>
2009-10-20 00:02:19 -0400

    Test cleanup.

--- test/gridfs_test.rb
+++ test/gridfs_test.rb
@@ -3,10 +3,6 @@ require 'test_helper'
 class Rack::GridFSTest < Test::Unit::TestCase
   include Rack::Test::Methods
   
-  def options_for_gridfs
-    { :hostname => 'myhostname.mydomain', :port => 8765, :database => 'mydatabase', :prefix => 'myprefix' }
-  end
-  
   def stub_mongodb_connection
     Rack::GridFS.any_instance.stubs(:connect!).returns(true)
   end
@@ -20,14 +16,14 @@ class Rack::GridFSTest < Test::Unit::TestCase
   end
   
   def app 
+    gridfs_opts = test_database_options
     Rack::Builder.new do
-      use Rack::GridFS, :hostname => 'localhost', :port => 27017, :database => 'test', :prefix => 'gridfs'
+      use Rack::GridFS, gridfs_opts
       run lambda { |env| [200, {'Content-Type' => 'text/plain'}, ["Hello, World!"]] }
     end
   end
 
   def load_artifact(filename, key, content_type)
-    GridFS::GridStore.unlink(db, key)
     GridFS::GridStore.open(db, key, 'w', :content_type => content_type) do |dest|
       File.open(File.join(File.dirname(__FILE__), 'artifacts', filename), 'r') do |orig|
         dest.puts orig.read
@@ -41,51 +37,52 @@ class Rack::GridFSTest < Test::Unit::TestCase
 
       setup do
         stub_mongodb_connection
+        @options = { :hostname => 'myhostname.mydomain', :port => 8765, :database => 'mydatabase', :prefix => 'myprefix' }
       end
 
       should "have a hostname option" do
-        mware = Rack::GridFS.new(nil, options_for_gridfs)
-        assert_equal options_for_gridfs[:hostname], mware.hostname
+        mware = Rack::GridFS.new(nil, @options)
+        assert_equal @options[:hostname], mware.hostname
       end
 
       should "have a default hostname" do
-        mware = Rack::GridFS.new(nil, options_for_gridfs.except(:hostname))
+        mware = Rack::GridFS.new(nil, @options.except(:hostname))
         assert_equal 'localhost', mware.hostname
       end
 
       should "have a port option" do
-        mware = Rack::GridFS.new(nil, options_for_gridfs)
-        assert_equal options_for_gridfs[:port], mware.port
+        mware = Rack::GridFS.new(nil, @options)
+        assert_equal @options[:port], mware.port
       end
 
       should "have a default port" do
-        mware = Rack::GridFS.new(nil, options_for_gridfs.except(:port))
+        mware = Rack::GridFS.new(nil, @options.except(:port))
         assert_equal Mongo::Connection::DEFAULT_PORT, mware.port
       end
 
       should "have a database option" do
-        mware = Rack::GridFS.new(nil, options_for_gridfs)
-        assert_equal options_for_gridfs[:database], mware.database
+        mware = Rack::GridFS.new(nil, @options)
+        assert_equal @options[:database], mware.database
       end
 
       should "not have a default database" do
-        mware = Rack::GridFS.new(nil, options_for_gridfs.except(:database))
+        mware = Rack::GridFS.new(nil, @options.except(:database))
         assert_nil mware.database
       end
 
       should "have a prefix option" do
-        mware = Rack::GridFS.new(nil, options_for_gridfs)
-        assert_equal mware.prefix, options_for_gridfs[:prefix]
+        mware = Rack::GridFS.new(nil, @options)
+        assert_equal mware.prefix, @options[:prefix]
       end
       
       should "have a default prefix" do
-        mware = Rack::GridFS.new(nil, options_for_gridfs.except(:prefix))
+        mware = Rack::GridFS.new(nil, @options.except(:prefix))
         assert_equal mware.prefix, 'gridfs'
       end
 
       should "connect to the MongoDB server" do
         Rack::GridFS.any_instance.expects(:connect!).returns(true).once
-        Rack::GridFS.new(nil, options_for_gridfs)
+        Rack::GridFS.new(nil, @options)
       end
 
     end
@@ -102,6 +99,10 @@ class Rack::GridFSTest < Test::Unit::TestCase
       setup do
         load_artifact('test.txt', 'test.txt', 'text/plain')
         load_artifact('test.html', 'test.html', 'text/html')
+      end
+
+      teardown do
+        db.collection('fs.files').clear
       end
 
       should "return TXT files stored in GridFS" do
